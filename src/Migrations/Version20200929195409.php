9772f0c4f8bc71ceef1568aabff80b9a42094dbb
Ley Gabriel <gabriel.ley@outlook.fr>
2020-09-29 21:57:57 +0200

    Fix migrations conflicts & update php packages (security issues)

--- src/Migrations/Version20200929195409.php
+++ src/Migrations/Version20200929195409.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200916162032 extends AbstractMigration
+final class Version20200929195409 extends AbstractMigration
 {
     public function getDescription() : string
     {
