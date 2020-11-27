fa4caf9ee9e3b4243713dc7063be50e7e2ef4cb7
Clara Cassel <clara.tiare@live.fr>
2020-11-27 17:39:24 +0100

    fix after PR

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,10 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
