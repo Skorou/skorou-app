4b448c8d1e2ac9712f527dc82308ccf628adfc5a
Clara Cassel <clara.tiare@live.fr>
2020-12-17 09:50:49 +0100

    template tree structure changed + style

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DashboardController extends AbstractController
