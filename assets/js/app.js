0b50d557b7e1b706e9681955d202b3e0a2a467d2
Dumorya <clara.tiare@live.fr>
2020-12-18 15:00:27 +0100

    fix unneeded file which broke the style

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@
 // any CSS you import will output into a single css file (app.scss in this case)
 import '../css/app.scss';
 import 'bootstrap';
-import bsCustomFileInput from 'bs-custom-file-input';
+// import bsCustomFileInput from 'bs-custom-file-input';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
