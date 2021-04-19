ae3f54aaa6483d994a94791a7e150517f8a53a10
Gabriel <gabriel.ley@outlook.fr>
2021-04-19 14:20:35 +0200

    Editor : snapping edges on shape drag

--- assets/js/TemplateEditor/Components/Canvas.vue
+++ assets/js/TemplateEditor/Components/Canvas.vue
@@ -5,6 +5,8 @@
         @mousedown="handleStageMouseDown"
         @mousemove="handleStageMouseMove"
         @mouseup="handleStageMouseUp"
+        @dragmove="handleDragMove"
+        @dragend="handleDragEnd"
     >
         <v-layer ref="mainLayer">
             <template v-for="element in elements">
@@ -18,6 +20,9 @@
 
 <script>
     import Element from "./Element";
+
+    const SNAPPING_GUIDELINE_OFFSET = 3;
+
     export default {
       name: "Canvas",
       components: {Element},
@@ -52,7 +57,7 @@
         updateTransformer(selectedElements) {
           const transformerNode = this.$refs.transformer.getNode();
 
-          const selectedNodes = selectedElements.map(el => el.VueComponent.getNode());
+          const selectedNodes = selectedElements.filter(el => el.VueComponent).map(el => el.VueComponent.getNode());
 
           transformerNode.nodes(selectedNodes);
 
@@ -168,6 +173,229 @@
             this.selectedElements.map(callback);
             this.$refs.mainLayer.getNode().batchDraw();
           }
+        },
+        /* Snap position of object (anchor) */
+        getLineGuidesStops(stage, skipShape) {
+          // Snap stage border and center of stage
+          let vertical = [0, stage.width() / 2, stage.width()];
+          let horizontal = [0, stage.height() / 2, stage.height()];
+
+          stage.find('.konvaElement').forEach(guideItem => {
+            if (guideItem === skipShape) {
+              return;
+            }
+
+            if(this.selectedElements.indexOf(guideItem) !== -1) {
+              return;
+            }
+
+            let box = guideItem.getClientRect();
+            vertical.push([box.x, box.x + box.width, box.x + box.width / 2]);
+            horizontal.push([box.y, box.y + box.height, box.y + box.height / 2]);
+          });
+
+          return {
+            vertical: vertical.flat(),
+            horizontal: horizontal.flat(),
+          };
+        },
+        getObjectSnappingEdges(node) {
+          let box = node.getClientRect();
+          let absPos = node.absolutePosition();
+          return {
+            vertical: [
+              {
+                guide: Math.round(box.x),
+                offset: Math.round(absPos.x - box.x),
+                snap: 'start'
+              },
+              {
+                guide: Math.round(box.x + box.width / 2),
+                offset: Math.round(absPos.x - box.x - box.width / 2),
+                snap: 'center'
+              },
+              {
+                guide: Math.round(box.x + box.width),
+                offset: Math.round(absPos.x - box.x - box.width),
+                snap: 'end'
+              },
+            ],
+            horizontal: [
+              {
+                guide: Math.round(box.y),
+                offset: Math.round(absPos.y - box.y),
+                snap: 'start'
+              },
+              {
+                guide: Math.round(box.y + box.height / 2),
+                offset: Math.round(absPos.y - box.y - box.height / 2),
+                snap: 'center'
+              },
+              {
+                guide: Math.round(box.y + box.height),
+                offset: Math.round(absPos.y - box.y - box.height),
+                snap: 'end'
+              },
+            ],
+          };
+        },
+        getGuides(lineGuidesStops, itemBounds) {
+          let resultV = [];
+          let resultH = [];
+          lineGuidesStops.vertical.forEach(lineGuide => {
+            itemBounds.vertical.forEach(itemBound => {
+              let diff = Math.abs(lineGuide - itemBound.guide);
+
+              if(diff < SNAPPING_GUIDELINE_OFFSET) {
+                resultV.push({
+                  lineGuide: lineGuide,
+                  diff: diff,
+                  snap: itemBound.snap,
+                  offset: itemBound.offset
+                });
+              }
+            });
+          });
+
+          lineGuidesStops.horizontal.forEach(lineGuide => {
+            itemBounds.horizontal.forEach(itemBound => {
+              let diff = Math.abs(lineGuide - itemBound.guide);
+              if(diff < SNAPPING_GUIDELINE_OFFSET) {
+                resultH.push({
+                  lineGuide: lineGuide,
+                  diff: diff,
+                  snap: itemBound.snap,
+                  offset: itemBound.offset
+                });
+              }
+            });
+          });
+
+          let guides = [];
+
+          let minV = resultV.sort((a, b) => a.diff - b.diff)[0];
+          let minH = resultH.sort((a, b) => a.diff - b.diff)[0];
+          if (minV) {
+            guides.push({
+              lineGuide: minV.lineGuide,
+              offset: minV.offset,
+              orientation: 'V',
+              snap: minV.snap
+            });
+          }
+          if (minH) {
+            guides.push({
+              lineGuide: minH.lineGuide,
+              offset: minH.offset,
+              orientation: 'H',
+              snap: minH.snap
+            });
+          }
+          return guides;
+        },
+        drawGuides(guides) {
+          const layer = this.$refs.mainLayer.getNode()
+          guides.forEach((lg) => {
+            if (lg.orientation === "H") {
+              let line = new Konva.Line({
+                points: [-6000, 0, 6000, 0],
+                stroke: 'rgb(0, 161, 255)',
+                strokeWidth: 1,
+                name: 'guid-line',
+                dash: [4, 6],
+              });
+              layer.add(line);
+              line.absolutePosition({
+                x: 0,
+                y: lg.lineGuide
+              });
+              layer.batchDraw();
+            } else if (lg.orientation === "V") {
+              let line = new Konva.Line({
+                points: [0, -6000, 0, 6000],
+                stroke: 'rgb(0, 161, 255)',
+                strokeWidth: 1,
+                name: "guid-line",
+                dash: [4, 6]
+              });
+              layer.add(line);
+              line.absolutePosition({
+                x: lg.lineGuide,
+                y: 0,
+              });
+              layer.batchDraw();
+            }
+          });
+        },
+        handleDragMove(e) {
+          const layer = e.target.getLayer();
+
+          if(this.selectedElements.length && e.target.getType() !== "Group"){
+            return
+          }
+
+          layer.find('.guid-line').destroy();
+
+          let lineGuideStops = this.getLineGuidesStops(e.target.getStage(), e.target);
+          let itemBounds = this.getObjectSnappingEdges(e.target);
+          let guides = this.getGuides(lineGuideStops, itemBounds);
+
+          if(!guides.length) {
+            return;
+          }
+
+          this.drawGuides(guides);
+
+          let absPos = e.target.absolutePosition();
+
+          guides.forEach(lg => {
+            switch(lg.snap) {
+              case 'start': {
+                switch(lg.orientation) {
+                  case 'V': {
+                    absPos.x = lg.lineGuide + lg.offset;
+                    break;
+                  }
+                  case 'H': {
+                    absPos.y = lg.lineGuide + lg.offset;
+                    break;
+                  }
+                }
+                break;
+              }
+              case 'center': {
+                switch (lg.orientation) {
+                  case 'V': {
+                    absPos.x = lg.lineGuide + lg.offset;
+                    break;
+                  }
+                  case 'H': {
+                    absPos.y = lg.lineGuide + lg.offset;
+                    break;
+                  }
+                }
+                break;
+              }
+              case 'end': {
+                switch(lg.orientation) {
+                  case 'V': {
+                    absPos.x = lg.lineGuide + lg.offset;
+                    break;
+                  }
+                  case 'H': {
+                    absPos.y = lg.lineGuide + lg.offset;
+                    break;
+                  }
+                }
+              }
+            }
+          });
+          e.target.absolutePosition(absPos);
+        },
+        handleDragEnd(e) {
+          const layer = e.target.getLayer();
+          layer.find('.guid-line').destroy();
+          layer.batchDraw();
         }
       }
     }
