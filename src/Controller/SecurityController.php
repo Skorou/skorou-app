c3677d71cb18bfd418bc2cca09f93a99c8f52385
Clara Cassel <clara.tiare@live.fr>
2021-06-27 18:26:24 +0200

    forgotten password + reset password + beginning of design

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    private $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
     /**
      * @Route("/login", name="app_login")
      *
@@ -52,7 +68,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/", name="index")
+     * @Route("/index", name="entry_point")
      */
     public function welcomePage()
     {
@@ -62,5 +78,112 @@ class SecurityController extends AbstractController
         }
 
         return $this->render('security/index.html.twig');
+    }
+
+    /**
+     * @Route("/forgotten_password", name="app_forgotten_password")
+     */
+    public function forgottenPassword(
+        Request $request,
+        TokenGeneratorInterface $tokenGenerator,
+        MailerInterface $mailer
+    ): Response
+    {
+        //redirect user if he/she is logged in and wants to go on this page
+        if ($this->getUser())
+        {
+            return $this->redirectToRoute('homepage');
+        }
+
+        if ($request->isMethod('POST'))
+        {
+            $email = $request->request->get('email');
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $user = $entityManager->getRepository(User::class)->findOneByEmail($email);
+            /* @var $user User */
+
+            if ($user === null)
+            {
+                $this->addFlash('danger', 'Email inconnu');
+                return $this->redirectToRoute('app_login');
+            }
+            $token = $tokenGenerator->generateToken();
+
+            try
+            {
+                $user->setResetToken($token);
+                $entityManager->flush();
+            }
+            catch (\Exception $e)
+            {
+                $this->addFlash('warning', $e->getMessage());
+                return $this->redirectToRoute('app_login');
+            }
+
+            $url = $this->generateUrl('app_reset_password', array('token' => $token), UrlGeneratorInterface::ABSOLUTE_URL);
+
+            $email = (new TemplatedEmail())
+                ->from('contact@skorou.com')
+                ->to($user->getEmail())
+                ->htmlTemplate('emails/forgotten_password.html.twig')
+                ->context([
+                    'url' => $url
+                ]);
+
+            try
+            {
+                $mailer->send($email);
+            }
+            catch (TransportExceptionInterface $e)
+            {
+                throw new Exception("Sending mail failed");
+            }
+
+            $this->addFlash('notice', 'E-mail envoyé');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/forgotten_password.html.twig');
+    }
+
+    /**
+     * @Route("/reset_password/{token}", name="app_reset_password")
+     */
+    public function resetPassword(Request $request, string $token, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        //redirect user if he/she is logged in and wants to go on this page
+        if ($this->getUser())
+        {
+            return $this->redirectToRoute('app_login');
+        }
+
+        if ($request->isMethod('POST'))
+        {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $user = $entityManager->getRepository(User::class)->findOneByResetToken($token);
+            /* @var $user User */
+
+            if ($user === null)
+            {
+                $this->addFlash('danger', 'Token inconnu');
+                return $this->redirectToRoute('app_login');
+            }
+
+            $user->setResetToken(null);
+            $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('password')));
+            $entityManager->flush();
+
+            $this->addFlash('notice', 'Mot de passe mis à jour');
+
+            return $this->redirectToRoute('app_login');
+        }
+        else
+        {
+            return $this->render('security/reset_password.html.twig', ['token' => $token]);
+        }
+
     }
 }
